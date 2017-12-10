80d82fa4d6419acb3ca85eb0e2bbf6d3c1f1f84e
Aguirre Sabino <aguirresabino187@gmail.com>
2017-12-10 03:24:50 -0300

    Implementando Dao de cliente para o banco de dados postgres

---
+++ AtividadePW1_JDBC_DAO/src/main/java/br/com/ifpb/dao/ClienteDaoPostgres.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ifpb.dao;
+
+import br.com.ifpb.model.Cliente;
+import java.util.List;
+
+/**
+ *
+ * @author aguirre
+ */
+public class ClienteDaoPostgres implements ClienteDaoIF{
+
+    @Override
+    public boolean incluir(Cliente o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean alterar(Cliente o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean excluir(Cliente o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Cliente> listar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
