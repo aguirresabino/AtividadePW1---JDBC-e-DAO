00d10eaf3770375d2cabdcca45523d49799a8f13
Aguirre Sabino <aguirresabino187@gmail.com>
2017-12-10 03:27:10 -0300

    Implementando interface PedidoDao e dao para pedido no banco de dados postgres

---
+++ AtividadePW1_JDBC_DAO/src/main/java/br/com/ifpb/dao/PedidoDaoIF.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ifpb.dao;
+
+import br.com.ifpb.model.Pedido;
+
+/**
+ *
+ * @author aguirre
+ */
+public interface PedidoDaoIF extends DaoGenericIF<Pedido>{
+    
+}
