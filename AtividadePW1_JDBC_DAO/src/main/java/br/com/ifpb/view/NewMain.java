c6b73065b630c150a47239bdef243c0472696a4c
Aguirre Sabino <aguirresabino187@gmail.com>
2017-12-10 22:26:40 -0300

    Implementando Classe principal para testar os métodos

---
+++ AtividadePW1_JDBC_DAO/src/main/java/br/com/ifpb/view/NewMain.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ifpb.view;
+
+import br.com.ifpb.controller.ClienteCtrl;
+import br.com.ifpb.controller.PedidoCtrl;
+import br.com.ifpb.model.Cliente;
+import br.com.ifpb.model.ClienteAtivoEnum;
+import br.com.ifpb.model.Pedido;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author aguirre
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ClienteCtrl clienteCtrl = new ClienteCtrl();
+        PedidoCtrl pedidoCtrl = new PedidoCtrl();
+        Cliente cliente = null;
+        Pedido pedido = null;
+        
+        System.out.println("Testando métodos implementados para Cliente e Pedido");
+        System.out.println("_____________________________________________________");
+        System.out.println("Cadastrando um Cliente");
+        
+        String nome, documento = null;
+        Double saldo = null;
+        System.out.print("Digite o nome do Cliente: ");
+        nome = scan.next();
+        System.out.print("Digite o documento do cliente: ");
+        documento = scan.next();
+        System.out.print("Digite o seu saldo: ");
+        saldo = scan.nextDouble();
+        
+        cliente = new Cliente(nome, documento, saldo, ClienteAtivoEnum.ATIVO);
+        if(clienteCtrl.incluir(cliente)){
+            System.out.println("Cadastro realizado com sucessso!");
+        }else{
+            System.out.println("Cadastro de cliente não realizado!");
+        }
+        System.out.println("_____________________________________________________");
+        
+        System.out.println("\nCadastrando um pedido para o usuário criado.");
+        pedido = new Pedido(LocalDate.now(), clienteCtrl.getClienteByDocumento(cliente.getDocumento()), 127.53);
+        if(pedidoCtrl.incluir(pedido)){
+            System.out.println("Pedido cadastrado!");
+        }else{
+            System.out.println("Pedido não foi cadastrado!");
+        }
+        System.out.println("_____________________________________________________");
+        
+        System.out.println("\nCadastrando outro pedido para o usuário criado.");
+        pedido = new Pedido(LocalDate.now(), clienteCtrl.getClienteByDocumento(cliente.getDocumento()), 5000);
+        if(pedidoCtrl.incluir(pedido)){
+            System.out.println("Pedido cadastrado!");
+        }else{
+            System.out.println("Pedido não foi cadastrado!");
+        }
+        System.out.println("_____________________________________________________");
+        
+        System.out.println("Listando todos os pedidos do cliente cadastrado");
+        System.out.println(pedidoCtrl.getPedidoByCliente(clienteCtrl.getClienteByDocumento(cliente.getDocumento())));
+        System.out.println("_____________________________________________________");
+
+        System.out.println("Alterando pedido cadastrado.");
+        
+        List<Pedido> pedidos = pedidoCtrl.getPedidoByCliente(clienteCtrl.getClienteByDocumento(cliente.getDocumento()));
+        
+        System.out.println("Escolha um pedido para alterar");
+        for(int i = 0; i < pedidos.size(); i++){
+            System.out.println(i + ". " + pedidos.get(i).toString());
+        }
+        int pedidoEscolhido = scan.nextInt();
+        
+        Pedido novoPedido = new Pedido(pedidos.get(pedidoEscolhido).getId(), LocalDate.now(), pedido.getCliente(), 777);
+        if(pedidoCtrl.alterar(novoPedido)){
+            System.out.println("O pedido foi alterado");
+        }else{
+            System.out.println("O pedido não foi alterado");
+        }
+        System.out.println("_____________________________________________________");        
+        
+        
+        System.out.println("Excluindo último pedido cadastrado para o usuário");
+        System.out.println("Escolha um pedido para excluir");
+        pedidos = pedidoCtrl.getPedidoByCliente(clienteCtrl.getClienteByDocumento(cliente.getDocumento()));
+        for(int i = 0; i < pedidos.size(); i++){
+            System.out.println(i + ". " + pedidos.get(i).toString());
+        }
+        pedidoEscolhido = scan.nextInt();
+        Pedido pedidoExcluir = pedidos.get(pedidoEscolhido);
+        if(pedidoCtrl.excluir(pedidoExcluir)){
+            System.out.println("Pedido excluído!");
+        }else{
+            System.out.println("O pedido não foi excluído");
+        }
+        System.out.println("_____________________________________________________");
+    }
+}
